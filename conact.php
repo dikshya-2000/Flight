4aa7bd506322233292d6dd20050ae41260d6b3e3
dikshya-2000 <106343560+dikshya-2000@users.noreply.github.com>
2022-09-03 23:29:59 +0530

    Airline Reservation System
    This website is created on Airline Reservation System using HTML, CSS, JavaScript, Bootstrap, PHP and MySQL. Users can book their seats and buy tickets free here. The admin monitors customer-related activities through the dashboard.

---
+++ conact.php
@@ -0,0 +1,19 @@
+<?php
+include('connect.php');
+session_start();
+$name= $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+echo $name,$email,$message;
+$sql= "INSERT INTO `contact`(`user_id`, `name`, `email`, `message`) VALUES ('', '$name', '$email', '$message')";
+$data= mysqli_query($conn,$sql);
+if ($data) 
+{
+	echo "<script>alert('Thank you for your support!')</script>";
+	echo "<script>window.location.href='index.php'</script>";
+}
+else
+{
+	echo "Not Inserted";
+}
+?>
